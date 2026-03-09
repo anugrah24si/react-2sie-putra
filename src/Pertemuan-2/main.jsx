8757695bb70e83eedbf5e60f309486430b240492
Anugrah Putra Fajar <anugrah24si@mahasiswa.pcr.ac.id>
2026-03-09 15:31:04 +0700

    first commit

---
+++ src/Pertemuan-2/main.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from "react-dom/client";
+import HelloWorld from "./HelloWorld";
+import QuoteText from "./HelloWorld";
+import Container from "./Container";
+import './custom.css';
+
+createRoot(document.getElementById("root"))
+    .render(
+        <div>
+
+            <Container>
+                <img src="img/wahyu.png" width="500" height="500" />
+                 <HelloWorld/>
+                 <QuoteText/>
+            </Container>
+            
+            
+        </div>
+    )
+
